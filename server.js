af06a2504ba13176c6b1b57fc53220d8086c1a01
mikkelkw <mikkelkwahl@gmail.com>
2019-12-09 08:14:35 -0700

    changes made to add sql server

--- server.js
+++ server.js
@@ -2,18 +2,41 @@ const express = require('express');
 const app = express();
 const path = require('path');
 var bodyParser = require('body-parser');
+var sql = require("mssql");
 
-app.use(bodyParser.urlencoded({ extended: true })); 
+var urlencodedparser =  bodyParser.urlencoded({ extended: false })
+//app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.use(express.static(__dirname));
 
-app.post('/submitmessage', function(req, res) {
-    res.send('You sent the name "' + req.body.name + '".');
-    console.log(req.body.name)
+app.post('/submitmessage', urlencodedparser, function(req, res) {
+    
+    
+    var mysql = require('mysql');
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "3560Hemsedal",
+    database: "contactus",
+    port: "1337"
   });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    var name = req.body.name;
+    var email = req.body.email;
+    var text = req.body.text
+    var sql = "insert into contact (name, email, text) values ('" + name + "', '" + email + "', '" + text + "');";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+      });
+      res.sendFile('index.html', {root: path.join(__dirname)});
+  });
+});
 
+var html_dir = './html/';
 app.get('/', function(req, res){
-    res.sendFile('index.html', {root: path.join(__dirname)});
+    res.sendFile(html_dir + 'index.html');
 })
 
 app.listen(8080, function(){
